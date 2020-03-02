4c4d7d2bdfb1baff7e645213d7491f566ea1677d
Dinkus <robertsnaer901@gmail.com>
2020-03-02 16:05:15 +0000

    If there is a god, IT'S ME!

--- script.js
+++ script.js
@@ -21,19 +21,19 @@ function fetchData() {
 				.catch(function (error) {
 						//When unsuccessful, print the error.
 						console.log(error);
-				})
-				.then(function () {
-						// This code is always executed, independent of whether the request succeeds or fails.
 				});
 }
 
 function generateGrid(data) {
+	// This function creates the game board.
 		this.grid.innerHTML="";
 		for (var i=0; i<data.board.rows; i++) {
 				row = this.grid.insertRow(i);
 				for (var j=0; j<data.board.cols; j++) {
 						cell = row.insertCell(j);
-						var string = i + "" + j;
+
+						// Create attributes for the cell
+						var string = i + "" + j;	// This is the id
 						cell.setAttribute("id", string);
 						var cla = document.createAttribute("class");
 						cla.value = "";
@@ -41,41 +41,55 @@ function generateGrid(data) {
 						var checked = document.createAttribute("checked");
 						checked.value = "false";
 						cell.setAttributeNode(checked);
+						var flagged = document.createAttribute("flagged");
+						flagged.value = "false";
+						cell.setAttributeNode(flagged);
 						var mine = document.createAttribute("mine");
 						mine.value = "false";
+
+						// Assigns appropriate value to the mine attribute if necessary
 						for (var k=0; k<data.board.minePositions.length; k++) {
 								if (i == data.board.minePositions[k][0] && j == data.board.minePositions[k][1]) {
 										mine.value = "true";
 								}
 						}
 						cell.setAttributeNode(mine);
-						cell.addEventListener("click", checkTile);
-						cell.addEventListener("contextmenu", flagTile);
+						cell.addEventListener("click", checkCell);
+						cell.addEventListener("contextmenu", flagCell);
 				}
 		}
 }
 
-function checkTile(e) {
+function checkCell(e) {
 	if (e.target.getAttribute("mine") == "true") {
 		revealMines();
 		console.log("BOMB")
 	}
+
 	else if (e.target.getAttribute("mine") == "false") {
 		console.log("NOT BOMB");
+		e.target.setAttribute("class", "td-clicked");
+		e.target.setAttribute("checked", "true");
 	}
 }
 
-function flagTile(e) {
+function flagCell(e) {
 	e.preventDefault();
 	var checked = e.target.getAttribute("checked");
-	if (checked == "false") {
+	var flagged = e.target.getAttribute("flagged");
+
+	if (checked == "false" && flagged == "false") {
 		var img = document.createElement("img");
 		img.src = "images/flag.png";
 		img.width = "20";
 		e.target.appendChild(img);
+		e.target.setAttribute("flagged", "true");
 	}
-	else if (checked == "true") {
 
+	else if (flagged == "true") {
+		var c = e.target.childNodes;
+		e.target.removeChild(c[0]);
+		e.target.setAttribute("flagged", "false");
 	}
 }
 
